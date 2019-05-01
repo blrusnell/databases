616282eec347c3517c27564e65c098fa09495cd2
blrusnell <blrusnell@gmail.com>
2019-05-01 18:55:22 -0500

    created our first table

--- server/schema.sql
+++ server/schema.sql
@@ -1,10 +1,32 @@
 CREATE DATABASE chat;
 
 USE chat;
+CREATE TABLE Users (
+  Id int NOT NULL AUTO_INCREMENT,
+  User char(15),
+  PRIMARY KEY (Id)
+);
+
+CREATE TABLE Rooms (
+  Id int NOT NULL AUTO_INCREMENT,
+  Room_name char(15),
+  PRIMARY KEY (Id)
+);
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  Id int NOT NULL AUTO_INCREMENT,
+  Message_Data char,
+  time_stamp TIMESTAMP,
+  Users_Id int,
+  Rooms_Id int,
+  PRIMARY  KEY (Id),
+  FOREIGN KEY (Users_Id) REFERENCES Users(Id),
+  FOREIGN KEY (Rooms_Id) REFERENCES Rooms(Id)
 );
+
+
+
 
 /* Create other tables and define schemas for them here! */
 
